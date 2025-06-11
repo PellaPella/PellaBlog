83027f1e54a6806f23bbf2f19720d824a1c22921
Pella <michaelpella33@gmail.com>
2025-06-11 14:16:08 +0800

    name

--- components/Header.jsx
+++ components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
             <div className="md:float-left block"> 
                 <Link href="/"> 
                     <span className="curser-pointer font-bold text-4xl text-white">
-                        Michael Pellegrini
+                        PellaBlog
                     </span>
                 </Link>
             </div>
